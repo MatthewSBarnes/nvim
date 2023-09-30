f625ad14e4a9ba43831b1d4b667712ee75160ce7
Louie Weng <louieweng@outlook.com>
2023-09-30 16:32:58 -0400

    fix keymap for fzf

--- lua/configs/keymaps.lua
+++ lua/configs/keymaps.lua
@@ -1,7 +1,7 @@
 vim.g.mapleader = " "
 
 vim.keymap.set('n', '<Leader>Ff', ':FzfLua files<Cr>')
-vim.keymap.set('n', '<Leader>FF', ':FzfLua files cwd=~/')
+vim.keymap.set('n', '<Leader>FF', ':FzfLua git_files<Cr>')
 vim.keymap.set('n', '<Leader>f', ':FzfLua grep_visual<Cr>')
 
 vim.keymap.set('n', '<Leader>q', ':quit!<Cr>')
