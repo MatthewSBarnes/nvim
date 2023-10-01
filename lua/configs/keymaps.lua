7e677aca4d04c22ad9c1a78b0d2bc9bf8b2e29d7
Louie Weng <louieweng@outlook.com>
2023-10-01 12:26:23 -0400

    add keymap to close terminal

--- lua/configs/keymaps.lua
+++ lua/configs/keymaps.lua
@@ -1,14 +1,16 @@
 vim.g.mapleader = " "
 
 vim.keymap.set('n', '<Leader>Ff', ':FzfLua files<Cr>')
-vim.keymap.set('n', '<Leader>FF', ':FzfLua git_files<Cr>')
+vim.keymap.set('n', '<Leader>Fg', ':FzfLua git_files<Cr>')
 vim.keymap.set('n', '<Leader>f', ':FzfLua grep_visual<Cr>')
 
 vim.keymap.set('n', '<Leader>q', ':quit!<Cr>')
-vim.keymap.set('n', '<Leader>t', ':Lexplore<Cr>')
+vim.keymap.set('n', '<Leader>t', ':terminal<Cr>')
 vim.keymap.set('n', '<Leader>w', ':write<Cr>')
 
 vim.keymap.set('n', '<C-[>', ':vertical resize -1<Cr>')
 vim.keymap.set('n', '<C-]>', ':vertical resize +1<Cr>')
 
 vim.keymap.set('n', '<Leader>?', ':WhichKey<Cr>')
+
+vim.keymap.set('n', '<C-space>','<C-\\><C-n><Cr>')
